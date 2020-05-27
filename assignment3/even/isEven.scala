0c9d0bf16bea52fdd5027dc821f436dd5a72be2e
georgeraveen <georgeraveen@gmail.com>
2020-05-27 17:04:56 +0000

    2

---
+++ assignment3/even/isEven.scala
@@ -0,0 +1,10 @@
+
+object isEven extends App{
+  def isEven(n:Int):Boolean= n match{
+    case 0 => true
+    case _ => isOdd(n-1)
+  }
+  def isOdd(n:Int):Boolean = !(isEven(n))
+  print(isEven(11))
+
+}
