7a94631ae253a04054e016dfb3e2cd4429fa820d
georgeraveen <georgeraveen@gmail.com>
2020-05-27 15:49:39 +0000

    new file:   prime.scala

---
+++ assignment3/prime/prime.scala
@@ -0,0 +1,25 @@
+
+object prime extends App{
+  def gcd(a:Int,b:Int):Int=b match{
+    case 0 => a
+    case x if x>a => gcd(x,a)
+    case _ => gcd(b,a%b)
+  }
+//  println(gcd(12,15));
+  def prime1(n:Int,i:Int=2):Boolean= i match {
+    case x if(x==n) => true
+    case x if gcd(n,x)>1 => false
+    case x => prime1(n,x+1)
+  }
+//  println(prime1(21));
+//
+//
+  def prime(n:Int,i:Int=2):Boolean= i match {
+    case x if(x>(n/2)) => true
+    case x if n%x==0 => false
+    case x => prime(n,x+1)
+  }
+
+  println(prime(5));
+  println(prime(8));
+}
