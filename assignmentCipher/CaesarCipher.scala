72749eafd2f1b3c1906546df019285ae90e78f81
georgeraveen <georgeraveen@gmail.com>
2020-07-18 10:19:17 +0000

    modified:   CaesarCipher.scala

--- assignmentCipher/CaesarCipher.scala
+++ assignmentCipher/CaesarCipher.scala
@@ -1,11 +1,11 @@
 object CaesarCipher extends App{
 
   println("Hello, Caesar Cipher!")
-  val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  val inputString = "Hello"
+  val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+  val inputString = "Hello welcome"
 
   val EncryptChar = (c:Char, shiftKey:Int, alpha:String) => alpha((alpha.indexOf(c.toUpper)+shiftKey) % alpha.size)
-  val DecryptChar = (c:Char, shiftKey:Int, alpha:String) => alpha((alpha.indexOf(c.toUpper)-shiftKey) % alpha.size)
+  val DecryptChar = (c:Char, shiftKey:Int, alpha:String) => alpha((alpha.indexOf(c.toUpper)-shiftKey+alpha.size) % alpha.size)
   val cipher = (algo:(Char,Int,String)=>Char,text:String,shiftKey:Int,alpha:String)=>text.map(algo(_,shiftKey,alpha))
 //  val cipher = (algo:(Char,Int,String)=>Char,text:String,shiftKey:Int,alpha:String)=>text.map(B=>algo(B,shiftKey,alpha))
   val Encrypt=cipher(EncryptChar,inputString,5,alphabet)
